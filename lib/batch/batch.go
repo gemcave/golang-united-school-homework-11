72167611495bbf78e3eae80cc6826212c4c8e093
yerowyn <a.yerzhanov@sapasoft.kz>
2022-07-08 11:22:12 +0600

    add: solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	res = make([]user, n)
+	ch_users := make(chan struct{}, pool)
+
+	for i := 0; i < int(n); i++ {
+		wg.Add(1)
+		ch_users <- struct{}{}
+
+		go func(i int64) {
+			defer wg.Done()
+			res[i] = getOne(i)
+			<-ch_users
+		}(int64(i))
+	}
+	wg.Wait()
+
+	return res
 }
